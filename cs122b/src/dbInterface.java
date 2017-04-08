e64b25969a7e60c24314818183717a9bb9b81041
deonma <deon.ma95@gmail.com>
2017-04-08 15:03:44 -0700

    Finished getMOvies

--- cs122b/src/dbInterface.java
+++ cs122b/src/dbInterface.java
@@ -1,4 +1,3 @@
-package cs122b;
 import java.sql.*;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -11,7 +10,7 @@ public class dbInterface
 	static final String USER = "root";
 	static final String PASS = "123456789";
 	public static void main(String[] args) {
-		
+		executeQuery();
 	}
 	public static String selectOption() {
 		Set<String> options = new HashSet<String>(Arrays.asList("a", "b", "c", "d", "e", "f", "s"));
@@ -38,37 +37,79 @@ public class dbInterface
 		}
 		
 	}
-	public void executeQuery() {
-		String option = "";
+	public static void executeQuery() {
 		Connection conn = null;
 		Statement stmt = null;
-		while(option != "s") {
-			option = selectOption();
-			switch(option) {
-				case "a":
-					break;
-				case "b":
-					break;
-				case "c":
-					break;
-				case "d":
-					break;
-				case "e":
-					break;
+		PreparedStatement ps = null;
+		try{
+			String option = "";
+
+			
+			while(option != "s") {
+				option = selectOption();
+				switch(option) {
+					case "a":
+						getMovieUI(ps, conn);
+						break;
+					case "b":
+						break;
+					case "c":
+						break;
+					case "d":
+						break;
+					case "e":
+						break;
+					
+				}
 				
 			}
-			
-		}
-
+		   }catch(SQLException se){
+			      //Handle errors for JDBC
+			      se.printStackTrace();
+			   }catch(Exception e){
+			      //Handle errors for Class.forName
+			      e.printStackTrace();
+			   }finally{
+			      //finally block used to close resources
+			      try{
+			         if(stmt!=null)
+			            stmt.close();
+			      }catch(SQLException se2){
+			      }// nothing we can do
+			      try{
+			         if(conn!=null)
+			            conn.close();
+			      }catch(SQLException se){
+			         se.printStackTrace();
+			      }//end finally try
+			   }//end try
+			   System.out.println("Goodbye!");
 		
 	}
-	public void getMovieUI() {
+	public static void getMovieUI(PreparedStatement ps, Connection conn) throws SQLException {
+		ResultSet rs = null;
 		while(true){
 			Scanner reader = new Scanner(System.in);
 			System.out.print("Enter Last Name and/or First Name or ID of Star::");
 			String star = reader.nextLine();
 			reader.close();
-			String result = sqlStatements.getMovies();
+			String result = sqlStatements.getMovies(star);
+			if (result == "f") {
+				System.out.println("Please Enter Valid Star");
+				continue;
+			}
+			ps = conn.prepareStatement(result);
+			rs = ps.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
+		    int columnsNumber = rsmd.getColumnCount();
+		    while (rs.next()) {
+		    	//Print one row          
+		    	for(int i = 1 ; i <= columnsNumber; i++){
+		    		System.out.print(rs.getString(i) + " "); //Print one element of a row
+		    	}
+		    	System.out.println();//Move to the next line to print the next row.           
+			}
+			
 		}
 		
 	}
